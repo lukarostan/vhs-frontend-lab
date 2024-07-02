bf347cdd956b81af43211386014a45f55860cb6d
lukar <luka.rostan@true-north.hr>
2024-07-02 15:57:09 +0200

    Add necessary api methods

--- vhs-frontend/src/services/api/api.ts
+++ vhs-frontend/src/services/api/api.ts
@@ -1,7 +1,24 @@
 import axios from "axios";
+import {TapeInputs} from "../../components/CreateTape";
 
-export async function get(url: string) {
-    const response = await axios.get(url)
+const API_URL = ' http://localhost:3000/api';
+
+export async function getAll() {
+    const response = await axios.get(`${API_URL}/vhs`)
     return response.data;
+}
 
+export async function getOne(id: string) {
+    const response = await axios.get(`${API_URL}/vhs/${id}`)
+    return response.data;
+}
+
+export async function create(payload: Partial<TapeInputs>) {
+    const response = await axios.post(`${API_URL}/vhs/`, payload)
+    return response.data;
+}
+
+export async function deleteOne(id: string) {
+    const response = await axios.delete(`${API_URL}/vhs/${id}`)
+    return response.data;
 }
